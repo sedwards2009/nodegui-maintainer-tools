cc8ff3a3e088abe42eb2ddb3b8425ccb1e74af01
Simon Edwards <simon@simonzone.com>
2021-08-21 20:46:58 +0200

    Support `QModelIndex`, `int`, `bool`

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nodegui-maintainer-tools
+
 Tools to help maintain NodeGui itself
+
+The main tool is used to generate binding code from input C++ method declarations. It is intended to be run from inside VSCode or other text editor as a "call out to shell" style filter which operates on the selection.
+
+The tool can run with `node dist/main.js` and its input should be piped in via stdin.
+
+An extension like "Edit with Shell" in VSCode should be used to call out to a shell script wrapper which invokes the node program itself from the right directory.
+
+The input should have the following format:
+```
+// CLASS: FooBarWidget
+// TODO: int add(int a, int b)
+// TODO: int subtract(int a, int b)
+
+```
+The first line defines the name of the class these method belong to. This name is used in the generated code. Each method signature should start with `// TODO: `. This means that `TODO` comments inside NodeGui files can be directly expanded.
+
+The result is the generated code, split up into the different code parts such as C++ wrap header, C++ wrapper body, Napi method declarations, and finally the TypeScript code. These parts have to be manually moved to the correct files.
+
+If any error occurs then it forms the whole output.
--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ import { default as getStdin } from 'get-stdin';
 
 const log = console.log.bind(console);
 
-// const sigRegex = /$\/\/\w+\s+\w+[(][^)]+[)]^/;
-const sigRegex = /\/\/(?<returnType>\w+)\s+(?<methodName>\w+)[(](?<args>[^)]*)\)/;
+const classNameRegex = /\/\/\s+CLASS:\s+(?<className>\w+)/;
+const sigRegex = /\/\/\s+TODO:\s+(?<returnType>\w+)\s+\&?(?<methodName>[A-Za-z0-9]+)[(](?<args>[^)]*)\)/;
 
-
+// ---- Argument types ----
 const SUPPORTED_ARG_TYPES = [
-  'GLenum', 'GLfloat', 'GLint', 'GLuint', 'GLsizei', 'GLclampf', 'GLboolean'
+  'GLenum', 'GLfloat', 'GLint', 'GLuint', 'GLsizei', 'GLclampf', 'GLboolean', 'const QModelIndex', 'int'
 ] as const;
 type ArgumentTypeName = typeof SUPPORTED_ARG_TYPES[number];
 
@@ -20,56 +20,78 @@ function isSupportedArgType(argName: string): argName is ArgumentTypeName {
   return (<readonly string[]>SUPPORTED_ARG_TYPES).includes(argName);
 }
 
+// List of argument types which need to be deferenced with `*` during the method call.
+const DEFERENCE_TYPES: ArgumentTypeName[] = [
+  'const QModelIndex'
+];
+
+// ---- Return types ----
 const SUPPORTED_RETURN_TYPES = [
-  'void', 'GLfloat', 'GLenum', 'GLboolean', 'GLint', 'GLuint', 'GLclampf', 'GLsizei'
+  'void', 'GLfloat', 'GLenum', 'GLboolean', 'GLint', 'GLuint', 'GLclampf', 'GLsizei', 'bool'
 ];
 type ReturnTypeName = typeof SUPPORTED_RETURN_TYPES[number];
 
 function isSupportedReturnType(returnName: string): returnName is ReturnTypeName {
-  return (<readonly string[]>SUPPORTED_ARG_TYPES).includes(returnName);
+  return (<readonly string[]>SUPPORTED_RETURN_TYPES).includes(returnName);
 }
 
 
-interface ExpandedMethod {
-  methodDeclaration: string;
-  napiInit: string;
-  methodBody: string;
-  tsBody: string;
+interface ExpandedMethodOkResult {
+  readonly type: 'ok',
+  readonly methodDeclaration: string;
+  readonly napiInit: string;
+  readonly methodBody: string;
+  readonly tsBody: string;
 }
 
-function expandMethod(className: string, signature: string): ExpandedMethod {
+interface ExpandedMethodFailedResult {
+  readonly type: 'failed',
+  readonly message: string;
+}
+
+type ExpandedMethodResult = ExpandedMethodOkResult | ExpandedMethodFailedResult;
+
+
+function expandMethod(className: string, signature: string): ExpandedMethodResult {
   const found = signature.match(sigRegex);
   if ( ! found) {
-    return null;
+    const result: ExpandedMethodFailedResult = {
+      type: 'failed',
+      message: `Signature didn't match regex for '${signature}'.`
+    };
+    return result;
   }
 
   const returnTypeString = found.groups.returnType;
   if ( ! isSupportedReturnType(returnTypeString)) {
-    return null;
+    const result: ExpandedMethodFailedResult = {
+      type: 'failed',
+      message: `Return type '${returnTypeString}' is unknown for signature '${signature}'.`
+    };
+    return result;
   }
   const returnType: ReturnTypeName = returnTypeString;
 
   const args = parseArgs(found.groups.args);
   for (const arg of args) {
     if ( ! isSupportedArgType(arg.type)) {
-      return null;
+      const result: ExpandedMethodFailedResult = {
+        type: 'failed',
+        message: `Argument type '${arg.type}' is unknown for signature '${signature}'.`
+      };
+      return result;
     }
   }
 
   const methodName = found.groups.methodName;
-
-  const methodDeclaration = `  Napi::Value ${methodName}(const Napi::CallbackInfo& info);`;
+  const methodDeclaration = `  Napi::Value ${methodName}(const Napi::CallbackInfo& info);
+`;
   const napiInit = `       InstanceMethod("${methodName}", &${className}Wrap::${methodName}),
 `;
   let methodBody = `
 Napi::Value ${className}Wrap::${methodName}(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
-
-  if (info.Length() != ${args.length}) {
-    Napi::TypeError::New(env, "Wrong number of arguments")
-        .ThrowAsJavaScriptException();
-  }
 `;
 
   for (let i=0; i<args.length; i++) {
@@ -85,10 +107,16 @@ Napi::Value ${className}Wrap::${methodName}(const Napi::CallbackInfo& info) {
       case 'GLenum':
       case 'GLint':
       case 'GLsizei':
+      case 'int':
         methodBody += `  ${arg.type} ${arg.name} = info[${i}].As<Napi::Number>().Int32Value();`;
         break;
       case 'GLuint':
         methodBody += `  ${arg.type} ${arg.name} = info[${i}].As<Napi::Number>().Uint32Value();`;
+        break;
+
+      case 'const QModelIndex':
+        methodBody += `  QModelIndexWrap* ${arg.name}Wrap = Napi::ObjectWrap<QModelIndexWrap>::Unwrap(info[${i}].As<Napi::Object>());
+  QModelIndex* ${arg.name} = ${arg.name}Wrap->getInternalInstance();`;
         break;
 
       default:
@@ -107,7 +135,13 @@ Napi::Value ${className}Wrap::${methodName}(const Napi::CallbackInfo& info) {
   }
 
   methodBody += `this->instance->${methodName}(`;
-  methodBody += args.map(arg => arg.name).join(', ');
+  methodBody += args.map(arg => {
+    if (DEFERENCE_TYPES.includes(arg.type)) {
+      return `*${arg.name}`;
+    } else {
+      return arg.name;
+    }
+  }).join(', ');
   methodBody += `);
 `;
 
@@ -126,6 +160,7 @@ Napi::Value ${className}Wrap::${methodName}(const Napi::CallbackInfo& info) {
       break;
 
     case 'GLboolean':
+    case 'bool':
       methodBody += `  return Napi::Boolean::New(env, result);`;
       break;
 
@@ -155,7 +190,11 @@ methodBody += `
       case 'GLint':
       case 'GLuint':
       case 'GLsizei':
+      case 'int':
         tsBody += `number`;
+        break;
+      case 'const QModelIndex':
+        tsBody += 'QModelIndex';
         break;
       default:
         throw new Error(`Unexpected argument type ${arg.name} while processing TypeScript.`);
@@ -169,6 +208,7 @@ methodBody += `
       break;
 
     case 'GLboolean':
+    case 'bool':
       tsBody += 'boolean';
       break;
 
@@ -199,11 +239,12 @@ methodBody += `
 
 `;
 
-  return { methodDeclaration, napiInit, methodBody, tsBody };
+  const result: ExpandedMethodOkResult = { type: 'ok', methodDeclaration, napiInit, methodBody, tsBody };
+  return result;
 }
 
 interface CppArgument {
-  type: string;
+  type: ArgumentTypeName;
   name: string;
 }
 
@@ -214,7 +255,10 @@ function parseArgs(args: string): CppArgument[] {
   for (const part of parts) {
     const argParts = part.trim().split(' ');
     const type = argParts.slice(0, argParts.length-1).join(' ');
-    const name = argParts[argParts.length-1];
+    let name = argParts[argParts.length-1];
+    if (name.startsWith('&')) {
+      name = name.substr(1);
+    }
     if (name != "") {
       result.push({type, name});
     }
@@ -223,11 +267,7 @@ function parseArgs(args: string): CppArgument[] {
   return result;
 }
 
-/**
- * @param {string} methodName
- * @returns {string}
- */
-function formatTSMethodName(methodName) {
+function formatTSMethodName(methodName: string): string {
   if (methodName.startsWith('gl')) {
     const body = methodName.substr(2);
     return body.substr(0, 1).toLowerCase() + body.substr(1);
@@ -237,60 +277,83 @@ function formatTSMethodName(methodName) {
 }
 
 
-function processMethods(className: string, methodStrList: string): void {
-  const methodList = methodStrList.split('\n').filter(line => line.trim() !== '');
-  let napiInitBlock = '';
-  let bodyBlock = '';
-  let tsBlock = '';
-  for (let i=0; i<methodList.length; i++) {
-    const result = expandMethod(className, methodList[i]);
-    if (result != null) {
-      const { methodDeclaration, napiInit, methodBody, tsBody } = result;
-      methodList[i] = methodDeclaration;
+function process(inputString: string): void {
+  const lines = inputString.split('\n');
 
-      if (napiInit != null) {
-        napiInitBlock = napiInitBlock + napiInit;
-      }
+  let lineNumber = 0;
 
-      if (methodBody != null) {
-        bodyBlock = bodyBlock + methodBody;
-      }
-      if (tsBody != null) {
-        tsBlock = tsBlock + tsBody;
-      }
+  while (lines[lineNumber].trim() === '' && lineNumber < lines.length) {
+    lineNumber++;
+  }
+  if (lineNumber === lines.length) {
+    log(`ERROR: No non-blank lines were found.`);
+    return;
+  }
+
+  const found = lines[lineNumber].match(classNameRegex);
+  if ( ! found) {
+    log(`ERROR: Unable to find CLASS comment. i.e. // CLASS: FooBarWidget`);
+    return;
+  }
+  const className = found.groups.className;
+  lineNumber++;
+
+  const cppDeclaration: string[] = [];
+  const napiInitBlock: string[] = [];
+  const bodyBlock: string[] = [];
+  const tsBlock: string[] = [];
+  for (; lineNumber<lines.length; lineNumber++) {
+    const trimLine = lines[lineNumber].trim()
+    if (trimLine === '') {
+      continue;
+    }
+
+    const result = expandMethod(className, trimLine);
+    switch (result.type) {
+      case 'ok':
+        const { methodDeclaration, napiInit, methodBody, tsBody } = <ExpandedMethodOkResult> result;
+        cppDeclaration.push(methodDeclaration);
+
+        if (napiInit != null) {
+          napiInitBlock.push(napiInit);
+        }
+
+        if (methodBody != null) {
+          bodyBlock.push(methodBody);
+        }
+        if (tsBody != null) {
+          tsBlock.push(tsBody);
+        }
+        break;
+
+      case 'failed':
+        log(`>>> ERROR: ${result.message}`);
+        return;
     }
   }
 
   log('//----------------------------------------------------');
   log('// C++ declaration');
-  log(methodList.join('\n'));
+  log(cppDeclaration.join(''));
   log('');
 
   log('//----------------------------------------------------');
   log('// Napi declaration');
-  log(napiInitBlock);
+  log(napiInitBlock.join(''));
   log('');
 
   log('//----------------------------------------------------');
   log('// C++ body');
-  log(bodyBlock);
+  log(bodyBlock.join(''));
   log('');
 
   log('//----------------------------------------------------');
   log('// TS body');
-  log(tsBlock);
+  log(tsBlock.join(''));
 }
-
-// processMethods('QOpenGLExtraFunctions', methodStrList2);
-
-// processMethods('QOpelGLExtraFunctions', `
-// //GLenum 	 glGetError()
-// `);
-
-
 
 async function main(): Promise<void> {
   const input = await getStdin();
-  log(`|${input}|`);
+  process(input);
 }
 main();
